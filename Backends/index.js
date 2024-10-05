3ce29969662ba9d30b292d8813f4e0fca0db6d98
Harash <Harashporiya>
2024-10-05 11:37:55 +0530

    dataBase connect

--- Backends/index.js
+++ Backends/index.js
@@ -1,7 +1,15 @@
+const { config } = require("dotenv");
 const express = require("express")
 const mongoose = require("mongoose")
 const app = express()
 const PORT = 5000;
+config()
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 app.get("/", async(req,res)=>{
     return res.send("Harash")
